89227a66643822bb51d179e7a957ca817e2c5487
Radviger <radviger@gmail.com>
2021-11-17 19:28:58 +0200

    Support new core events.

--- src/main/java/ru/craftlogic/combat/CombatManager.java
+++ src/main/java/ru/craftlogic/combat/CombatManager.java
@@ -1,27 +1,30 @@
 package ru.craftlogic.combat;
 
 import com.google.gson.JsonObject;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketSoundEffect;
-import net.minecraft.util.CombatTracker;
 import net.minecraft.util.JsonUtils;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.craftlogic.api.CraftAPI;
-import ru.craftlogic.api.event.player.PlayerEnterCombat;
-import ru.craftlogic.api.event.player.PlayerExitCombat;
 import ru.craftlogic.api.event.player.PlayerTeleportHomeEvent;
+import ru.craftlogic.api.event.player.PlayerTeleportReplyEvent;
 import ru.craftlogic.api.event.player.PlayerTeleportRequestEvent;
+import ru.craftlogic.api.event.player.PlayerTimedTeleportEvent;
+import ru.craftlogic.api.server.PlayerManager;
 import ru.craftlogic.api.server.Server;
 import ru.craftlogic.api.text.Text;
 import ru.craftlogic.api.util.ConfigurableManager;
@@ -30,15 +33,17 @@ import ru.craftlogic.common.command.CommandManager;
 import ru.craftlogic.warps.event.PlayerWarpEvent;
 
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CombatManager extends ConfigurableManager {
     private static final Logger LOGGER = LogManager.getLogger("combatManager");
 
     private boolean enabled;
-    private final Set<UUID> inCombat = new HashSet<>();
+    private final Map<UUID, AtomicInteger> timers = new HashMap<>();
 
     public CombatManager(Server server, Path settingsDirectory) {
         super(server, settingsDirectory.resolve("combat.json"), LOGGER);
@@ -73,39 +78,26 @@ public class CombatManager extends ConfigurableManager {
         }
     }
 
-    @SubscribeEvent
-    public void onPlayerEnterCombat(PlayerEnterCombat event) {
-        EntityPlayer player = event.getEntityPlayer();
-        CombatTracker tracker = player.getCombatTracker();
-        EntityLivingBase attacker = tracker.getBestAttacker();
-        if (attacker instanceof EntityPlayer && attacker != player) {
-            EntityPlayer p = (EntityPlayer) attacker;
-            enterCombat(player);
-            enterCombat(p);
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onEntityAttack(AttackEntityEvent event) {
+        if (!event.isCanceled()) {
+            EntityPlayer player = event.getEntityPlayer();
+            Entity target = event.getTarget();
+            if (target instanceof EntityPlayer && !target.world.isRemote) {
+                enterCombat(player);
+                enterCombat(((EntityPlayer) target));
+            }
         }
     }
 
     private void enterCombat(EntityPlayer player) {
-        if (inCombat.add(player.getGameProfile().getId())) {
-            TextComponentTranslation message = new TextComponentTranslation("tooltip.combat.enter");
-            message.getStyle().setColor(TextFormatting.YELLOW);
-            player.sendStatusMessage(message, true);
-            playSound((EntityPlayerMP) player, CraftCombat.SOUND_ENTER, 0.8F, 1F);
-        }
-    }
-
-    @SubscribeEvent
-    public void onPlayerExitCombat(PlayerExitCombat event) {
-        EntityPlayer player = event.getEntityPlayer();
-        CombatTracker tracker = player.getCombatTracker();
-        EntityLivingBase attacker = tracker.getBestAttacker();
-        if (attacker instanceof EntityPlayerMP && attacker != player) {
-            EntityPlayer p = (EntityPlayer) attacker;
-            if (inCombat.remove(p.getGameProfile().getId())) {
-                TextComponentTranslation message = new TextComponentTranslation("tooltip.combat.exit");
+        if (!player.capabilities.disableDamage) {
+            UUID id = player.getGameProfile().getId();
+            if (timers.put(id, new AtomicInteger(300)) == null) {
+                TextComponentTranslation message = new TextComponentTranslation("tooltip.combat.enter");
                 message.getStyle().setColor(TextFormatting.YELLOW);
-                p.sendStatusMessage(message, true);
-                playSound((EntityPlayerMP) player, CraftCombat.SOUND_EXIT, 0.8F, 1F);
+                player.sendStatusMessage(message, true);
+                playSound((EntityPlayerMP) player, CraftCombat.SOUND_ENTER, 0.8F, 1F);
             }
         }
     }
@@ -113,14 +105,45 @@ public class CombatManager extends ConfigurableManager {
     @SubscribeEvent
     public void onPlayerExit(PlayerEvent.PlayerLoggedOutEvent event) {
         EntityPlayer player = event.player;
-        if (!player.world.isRemote && inCombat.remove(player.getGameProfile().getId())) {
-            CombatTracker tracker = player.getCombatTracker();
-            EntityLivingBase attacker = tracker.getBestAttacker();
-            if (attacker instanceof EntityPlayerMP && attacker != player) {
+        if (!player.world.isRemote) {
+            if (isInCombat(player.getGameProfile().getId())) {
                 playSound((EntityPlayerMP) player, CraftCombat.SOUND_ENTER, 0.8F, 1F);
                 player.attackEntityFrom(CraftCombat.PUNISHMENT, Float.MAX_VALUE);
             }
         }
+    }
+
+    @SubscribeEvent
+    public void serverTickEvent(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            PlayerManager playerManager = server.getPlayerManager();
+            Iterator<Map.Entry<UUID, AtomicInteger>> iterator = timers.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<UUID, AtomicInteger> entry = iterator.next();
+                Player player = playerManager.getOnline(entry.getKey());
+                if (player != null) {
+                    AtomicInteger timer = entry.getValue();
+                    if (timer.decrementAndGet() <= 0) {
+                        iterator.remove();
+                        exitCombat(player);
+                    }
+                } else {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    private void exitCombat(Player player) {
+        TextComponentTranslation message = new TextComponentTranslation("tooltip.combat.exit");
+        message.getStyle().setColor(TextFormatting.YELLOW);
+        player.sendStatus(message);
+        player.playSound(CraftCombat.SOUND_EXIT, 0.8F, 1F);
+    }
+
+    private boolean isInCombat(UUID id) {
+        AtomicInteger timer = timers.get(id);
+        return timer != null && timer.get() > 0;
     }
 
     private void playSound(EntityPlayerMP player, SoundEvent sound, float volume, float pitch) {
@@ -143,11 +166,24 @@ public class CombatManager extends ConfigurableManager {
         checkTeleport(event, event.player);
     }
 
+    @SubscribeEvent
+    public void onTeleportReply(PlayerTeleportReplyEvent event) {
+        if (event.targetAccepted) {
+            checkTeleport(event, event.player);
+            checkTeleport(event, event.target);
+        }
+    }
+
+    @SubscribeEvent
+    public void onTimedTeleport(PlayerTimedTeleportEvent event) {
+        checkTeleport(event, event.player);
+    }
+
     private void checkTeleport(net.minecraftforge.event.entity.player.PlayerEvent event, Player player) {
-        if (inCombat.contains(player.getId())) {
+        if (isInCombat(player.getId())) {
             event.setCanceled(true);
             player.sendMessage(Text.translation("chat.combat.no-teleport").red());
-            playSound((EntityPlayerMP) event.getEntityPlayer(), CraftCombat.SOUND_WARN, 0.8F, 1F);
+            player.playSound(CraftCombat.SOUND_WARN, 0.8F, 1F);
         }
     }
 }
